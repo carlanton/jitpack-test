7f467775d3acec02f5a668b915103cbefc3d024c
Anton Lindström <carlantonlindstrom@gmail.com>
2017-07-11 23:28:16 +0200

    Add lombok.config

--- src/main/java/io/github/carlanton/demo/Thing.java
+++ src/main/java/io/github/carlanton/demo/Thing.java
@@ -1,10 +1,18 @@
 package io.github.carlanton.demo;
 
 import lombok.Value;
+import lombok.val;
+
+import java.util.ArrayList;
 
 @Value
 public class Thing {
     private String a;
     private String b;
     private String c;
+
+    public void wow() {
+        val t = new ArrayList<String>();
+        System.err.println(t);
+    }
 }
